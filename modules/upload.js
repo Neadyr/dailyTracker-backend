626cdf49315bc293a4169c9b265e1bf8f9d714fe
Clo <Battello.clovis@gmail.com>
2025-07-24 09:46:55 +0200

    changed upload method to upload_stream

--- modules/upload.js
+++ modules/upload.js
@@ -4,22 +4,17 @@ const uniqid = require("uniqid");
 const streamifier = require("streamifier");
 
 const cloudinaryFileUpload = async (file) => {
-  //
-  //  const photoPath = `./tmp/${uniqid()}`;
-  // const hasBeenMoved = await file.mv(photoPath);
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      { resource_type: "auto" }, // any upload options you need
+      (error, result) => {
+        if (error) return reject(error);
+        resolve(result.secure_url); // <-- resolve with your HTTPS URL
+      }
+    );
 
-  // if (!hasBeenMoved) {
-  const stream = await cloudinary.uploader.upload_stream((error, result) => {
-    if (error) return reject(error);
-    resolve(result.secure_url);
+    streamifier.createReadStream(file.data).pipe(uploadStream);
   });
-
-  streamifier.createReadStream(file).pipe(stream);
-
-  return stream.secure_url;
-  // } else {
-  //   return { return: false, error: hasBeenMoved };
-  // }
 };
 
 module.exports = { cloudinaryFileUpload };
